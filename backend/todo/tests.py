dcb7f09d3d51da49cb66761b9892e3a72d2204e7
Yurley Sanchez <yurs.ksf1@gmail.com>
2021-06-09 20:43:08 -0500

    use asseert in

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from todo   .models import Todo
 
-# Create your tests here.
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(title="learn python", description="study every day python", completed=True)
+        Todo.objects.create(title="undertand django", description="practice create web application", completed=True)
+        Todo.objects.create(title="unitest class", description="keep attention in class about unites", completed=False)
+
+    def test_todos_complete(self):
+        """Todos that can speak are correctly identified"""
+        all_todos = Todo.objects.all()
+        self.assertEqual(len(all_todos), 3)
+
+        complete_todos = Todo.objects.filter(completed=True).all()
+        self.assertEqual(len(complete_todos), 2)
+
+        incomplete_todos = Todo.objects.filter(completed=True).all()
+        self.assertEqual(len(incomplete_todos), 1)
